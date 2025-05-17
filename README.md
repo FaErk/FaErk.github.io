057258f22eccd9a04abd0e5deb90f0b6448c988f
Fanny Erkhammar <faerkhammar@gmail.com>
2025-05-17 18:05:01 +0200

    Revert "new website version"
    This reverts commit 5e2933cd7a40a2b31a614d1b808feadd41c0f3a2.

--- README.md
+++
@@ -1,4 +0,0 @@
-# portfolio_website
-
-Created with the help of this tutorial by How to Become a Developer on YouTube: 
-https://www.youtube.com/watch?v=ldwlOzRvYOU&ab_channel=HowtoBecomeaDeveloper
--- script.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-elements");
-    const icon = document.querySelector(".hamburger-icon");
-
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
